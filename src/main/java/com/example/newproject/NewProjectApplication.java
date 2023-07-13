e7d18ecf8ad7898e1f78e3ea29cb3e4211ee34a6
StasyanOi <stan.est.high@gmail.com>
2023-07-13 23:25:31 +0300

    mutated an endpoint

--- src/main/java/com/example/newproject/NewProjectApplication.java
+++ src/main/java/com/example/newproject/NewProjectApplication.java
@@ -11,7 +11,7 @@ public class NewProjectApplication {
 
     @GetMapping
     public String getHelloWorld() {
-        return "Hello world finally from docker!";
+        return "Hello world from docker!";
     }
 
     public static void main(String[] args) {
